03ce954428f0e4813592c7d1a81e0bf7fa7f6107
Xenia <lommy101@gmail.com>
2019-12-28 18:56:33 +0900

    count setting completed..

--- Rate of Exchange Crawling.py
+++ Rate of Exchange Crawling.py
@@ -2,8 +2,8 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import numpy as np
 
-ex_time_list  = []
 exchange_list = []
+ex_time_list  = []
 
 for page_num in range(1,2):
     URL = "https://finance.naver.com/marketindex/exchangeDailyQuote.nhn?marketindexCd=FX_USDKRW&page={}".format(page_num)
@@ -13,9 +13,10 @@ for page_num in range(1,2):
     for x in range(len(soup.find_all(class_="num"))):
         if x%2 is 0:
             exchange_list.append(float(soup.find_all(class_="num")[x].text.replace(',','')))
-        print(soup.find_all(class_="date")[x].text)
-        
+    for x in range(len(soup.find_all(class_="date"))):
+        ex_time_list.append(soup.find_all(class_="date")[x].text)
+                
 exchange_list = np.array(exchange_list)
 ex_time_list = np.array(ex_time_list)
-print(exchange_list)
+print(exchange_list.shape)
 print(ex_time_list)
